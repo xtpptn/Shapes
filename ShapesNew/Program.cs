61ce4f22b0acd0a2ac01adf0236f4401b334fee6
George Si <georgij.sibajev@gmail.com>
2020-04-14 22:39:35 +0100

    Unsaved work update

--- ShapesNew/Program.cs
+++ ShapesNew/Program.cs
@@ -7,6 +7,26 @@ namespace ShapesNew
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(4, 6);
+            Rectangle square = new Rectangle(5);
+
+            circle.CalculateArea();
+            circle.CalculatePerimeter();
+            Console.WriteLine(circle.Area);
+            Console.WriteLine(circle.Perimeter);
+
+            rectangle.CalculateArea();
+            rectangle.CalculatePerimeter();
+            Console.WriteLine(rectangle.Area);
+            Console.WriteLine(rectangle.Perimeter);
+
+            square.CalculateArea();
+            square.CalculatePerimeter();
+            Console.WriteLine(square.Area);
+            Console.WriteLine(square.Perimeter);
+
         }
     }
 }
--- ShapesVirtual/Geometry.cs
+++ ShapesVirtual/Geometry.cs
@@ -6,19 +6,69 @@ namespace ShapesVirtual
 {
     public class Geometry
     {
+        public double Area { get; protected set; }
+        public double Perimeter { get; protected set; }
+
+        public virtual void CalculateArea()
+        {
+            Console.WriteLine("You will need to Override the base method");
+        }
+        public virtual void CalculatePerimeter()
+        {
+            Console.WriteLine("You will need to Override the base method");
+        }
     }
 
     public class Circle : Geometry
     {
 
-        
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
 
+        private double Radius { get; set; }
+
+
+        public override void CalculateArea()
+        {
+            this.Area = Radius * Radius * Math.PI;
+
+        }
+
+        public override void CalculatePerimeter()
+        {
+            this.Perimeter = 2 * Radius * Math.PI;
+        }
     }
+
 
     public class Rectangle : Geometry
     {
+        public Rectangle(double length, double width)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
 
-     
+        public Rectangle(double side)
+        {
+            this.Length = side;
+            this.Width = side;
+        }
+
+        private double Length { get; set; }
+        private double Width { get; set; }
+
+
+        public override void CalculateArea()
+        {
+            this.Area = Length * Width;
+        }
+
+        public override void CalculatePerimeter()
+        {
+            this.Perimeter = (2 * Length) + (2 * Width);
+        }
     }
-
 }
